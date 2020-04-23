dc93a0fe1285b467f277ba5eea622569900d29fe
peterhuyxuan <peter.hx.nguyen@gmail.com>
2020-04-23 18:47:46 +1000

    Updates

---
+++ precache-manifest.d305657b5d776bf234c532947741b74f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c72e6db96315ae53fc19",
+    "url": "/site/static/css/main.805e6675.chunk.css"
+  },
+  {
+    "revision": "c72e6db96315ae53fc19",
+    "url": "/site/static/js/main.c72e6db9.chunk.js"
+  },
+  {
+    "revision": "c4b54bfd49c02f9eaead",
+    "url": "/site/static/js/runtime~main.c4b54bfd.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/site/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "d27693df583f63f0088edc5d384b2214",
+    "url": "/site/index.html"
+  }
+];
