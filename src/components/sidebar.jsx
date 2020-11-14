b2833450a5742c7e91c115143a3a06ddfc315000
Peter Nguyen <23422588+peterhuyxuan@users.noreply.github.com>
2020-11-14 21:32:40 +1100

    Resolved issues with unneeded Bootstrap animation

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -30,23 +30,25 @@ export default class Sidebar extends Component {
             </div>
             <br />
             <nav id="colorlib-main-menu" role="navigation" className="navbar">
-              <ul>
-                <li className="active">
-                  <a href="#home" data-nav-section="home">
-                    Introduction
-                  </a>
-                </li>
-                <li>
-                  <a href="#about" data-nav-section="about">
-                    About
-                  </a>
-                </li>
-                <li>
-                  <a href="#timeline" data-nav-section="timeline">
-                    Timeline
-                  </a>
-                </li>
-              </ul>
+              <div id="navbar" className="collapse">
+                <ul>
+                  <li className="active">
+                    <a href="#home" data-nav-section="home">
+                      Introduction
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#about" data-nav-section="about">
+                      About
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#timeline" data-nav-section="timeline">
+                      Timeline
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </nav>
             <nav id="colorlib-main-menu">
               <ul>
